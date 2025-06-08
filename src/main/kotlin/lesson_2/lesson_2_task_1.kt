eccc4cea201db365f4391727fbf567444c54c5ff
Edward <vikuloveduard5@gmail.com>
2025-06-08 15:15:41 +0300

    KS-2-1 variables were added and an expression with output to the console was compiled

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val grade1 = 3
+    val grade2 = 4
+    val grade3 = 3
+    val grade4 = 5
+
+    val result = (grade1 + grade2 + grade3 + grade4) / 4.0
+
+    println(result)
+}
+
