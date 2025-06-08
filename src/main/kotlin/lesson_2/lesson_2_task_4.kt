96c8b01d09b6199e0cfb49d82343202b5c97986f
Edward <vikuloveduard5@gmail.com>
2025-06-08 16:15:32 +0300

    KS-2-4 create bonus units

---
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val minedCrystals = 7
+    val minedFerum = 11
+
+    val bonusCrystals = (minedCrystals * 20) / 100
+    val bonusFerum = (minedFerum * 20) / 100
+
+    println("Кристалическая руда: $bonusCrystals")
+    println("Железная руда: $bonusFerum")
+
+}
