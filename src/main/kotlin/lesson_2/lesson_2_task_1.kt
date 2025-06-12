c090eef99453bb0d23ee94bc84bb046e5580d24d
Edward <vikuloveduard5@gmail.com>
2025-06-12 12:45:19 +0300

    KS-2-1 Исправлены замечания

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -3,8 +3,10 @@ fun main() {
     val grade2 = 4
     val grade3 = 3
     val grade4 = 5
+    val numberOfStudents = 4
 
-    val result = (grade1 + grade2 + grade3 + grade4) / 4.0
+    val totalGrades = (grade1 + grade2 + grade3 + grade4)
+    val result = totalGrades.toFloat() / numberOfStudents
 
     println(result)
 }
